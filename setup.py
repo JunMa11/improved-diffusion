652d70a4023ac41500b15d0bf49d27a7fbbc2c94
Jun <1259389904@qq.com>
2022-12-21 13:19:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="improved-diffusion",
     py_modules=["improved_diffusion"],
-    install_requires=["blobfile>=1.0.5", "torch", "pandas", "scitkit-image", "matplotlib", "scipy"],
+    install_requires=["blobfile>=1.0.5", "torch", "pandas", "scitkit-image", "matplotlib", "scipy", "scikit-learn", "Pillow", "einops", "nibabel"],
 )
